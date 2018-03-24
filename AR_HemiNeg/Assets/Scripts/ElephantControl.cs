182e9bfa38af228f166b74bdca4ce74429bd61e2
ckauth <christian.kauth@live.com>
2018-03-24 13:04:50 +0100

    Added collision detection between player (blue cube) and playground (elephant)

---
+++ AR_HemiNeg/Assets/Scripts/ElephantControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElephantControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //GetComponent<Renderer>().enabled = false;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+
+    }
+    void OnTriggerExit(Collider col)
+    {
+        this.GetComponent<Renderer>().enabled = true;
+    }
+}
